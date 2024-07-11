f0693a3886971e68a08efd17e99a624cb39e1aef
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2024-07-11 18:38:37 +0300

    fix arch env and aws session manager

--- setup/aws.sh
+++ setup/aws.sh
@@ -6,13 +6,11 @@ function install_awscli() {
   if [[ "$OS" == "darwin" ]]; then
     $CURL_CMD -o $temp_dir/AWSCLIV2.pkg https://awscli.amazonaws.com/AWSCLIV2.pkg
     sudo installer -pkg $temp_dir/AWSCLIV2.pkg -target /
-    return
   fi
   if [[ "$OS" == "linux" ]]; then
     $CURL_CMD -o $temp_dir/awscliv2.zip https://awscli.amazonaws.com/awscli-exe-${OS}-${ARCHM}.zip
     $UNZIP_CMD $temp_dir $temp_dir/awscliv2.zip
     sudo $temp_dir/aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-    return
   fi
   rm -rf $temp_dir
 }
@@ -24,11 +22,9 @@ function install_aws_session_manager_plugin() {
     $CURL_CMD -o $temp_dir/sessionmanager-bundle.zip https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_${ARCHM}/sessionmanager-bundle.zip
     $UNZIP_CMD $temp_dir $temp_dir/sessionmanager-bundle.zip
     sudo $temp_dir/sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
-    return
   fi
-  if [[ "$OS" == "linux" ]]; then
-    # https://docs.aws.amazon.com/systems-manager/latest/userguide/install-plugin-linux.html
-    return
+  if [[ "$OS" == "linux" && "$DISTRO" == "fedora" ]]; then
+    sudo dnf install -y https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_${ARCHS}/session-manager-plugin.rpm
   fi
   rm -rf $temp_dir
 }
