da63c614f8e53dd8f3a4033442136e599eb7c344
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2025-04-27 14:55:55 +0300

    fix eza install

--- setup/eza.sh
+++ setup/eza.sh
@@ -10,8 +10,8 @@ function install_eza() {
 
   # anything else
   local remote_version=$(echo $(_curl_github https://api.github.com/repos/eza-community/eza/releases/latest) | jq -r '.tag_name')
-  local local_version=$(command -v eza &>/dev/null && fzf --version | awk '{print $1}' || echo "0.0.0")
-  local download_url="https://github.com/eza-community/eza/releases/download/$remote_version/eza_${ARCH}-unknown-${OS}-gnu.tar.gz"
+  local local_version=$(command -v eza &>/dev/null && eza --version | grep -oP '^v[0-9]+\.[0-9]+\.[0-9]+' || echo "v0.0.0")
+  local download_url="https://github.com/eza-community/eza/releases/download/$remote_version/eza_${ARCHM}-unknown-${OS}-gnu.tar.gz"
 
   local bin_name="eza"
   if [[ "${remote_version:1}" == "$local_version" ]]; then
