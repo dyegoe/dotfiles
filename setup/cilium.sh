eef2dc28790dbe398427154f8924cbd8627c895d
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2025-12-14 15:03:53 +0200

    feat(cilium): add setup functions for cilium and hubble completions
    - implemented setup_cilium function to configure cilium completion
    - added setup_hubble function to configure hubble completion
    - ensured completion files are created with appropriate permissions
    - updated zshrc to include op completion if op is installed

--- setup/cilium.sh
+++ setup/cilium.sh
@@ -1,3 +1,16 @@
+# ##### Setup cilium #####
+setup_commands+="setup_cilium "
+function setup_cilium() {
+  if command -v cilium &>/dev/null; then
+    local cilium_completion_file="$ZDOTDIR/functions/_cilium"
+    log_info "Setup cilium..."
+    mkdir -p $ZDOTDIR/functions
+    cilium completion zsh >$cilium_completion_file
+    chmod 755 $cilium_completion_file
+    log_info "  setup done..."
+  fi
+}
+
 # ##### Install cilium #####
 install_commands+="install_cilium "
 function install_cilium() {
@@ -13,6 +26,19 @@ function install_cilium() {
   log_info "  installing..."
   download_tar_gz_local_bin $download_url $bin_name
   log_info "  installed..."
+}
+
+# ##### Setup hubble #####
+setup_commands+="setup_hubble "
+function setup_hubble() {
+  if command -v hubble &>/dev/null; then
+    local hubble_completion_file="$ZDOTDIR/functions/_hubble"
+    log_info "Setup hubble..."
+    mkdir -p $ZDOTDIR/functions
+    hubble completion zsh >$hubble_completion_file
+    chmod 755 $hubble_completion_file
+    log_info "  setup done..."
+  fi
 }
 
 # ##### Install cilium hubble #####
