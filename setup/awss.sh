c3e8d75fe3805c4363de43a90b891ef2c002a182
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2026-04-05 09:39:23 +0300

    fix(awss, eza): correct version comparison logic
    - updated local version assignment in awss installation script
    - fixed version comparison logic in eza installation script

--- setup/awss.sh
+++ setup/awss.sh
@@ -3,7 +3,7 @@ install_commands+="install_awss "
 function install_awss() {
   log_info "Install awss..."
   local remote_version=$(echo $(_curl_github https://api.github.com/repos/dyegoe/awss/releases/latest) | jq -r '.tag_name')
-  local local_version=v$(command -v awss &>/dev/null && awss --version | awk '{print $3}' || echo "0.0.0")
+  local local_version=$(command -v awss &>/dev/null && awss --version | awk '{print $3}' || echo "v0.0.0")
   local download_url="https://github.com/dyegoe/awss/releases/download/$remote_version/awss-$remote_version-$OS-$ARCH.tar.gz"
   local bin_name="awss"
   if [[ "$remote_version" == "$local_version" ]]; then
--- setup/eza.sh
+++ setup/eza.sh
@@ -14,7 +14,7 @@ function install_eza() {
   local download_url="https://github.com/eza-community/eza/releases/download/$remote_version/eza_${ARCHM}-unknown-${OS}-gnu.tar.gz"
 
   local bin_name="eza"
-  if [[ "${remote_version:1}" == "$local_version" ]]; then
+  if [[ "${remote_version}" == "$local_version" ]]; then
     log_info "  is up to date..."
     return
   fi
